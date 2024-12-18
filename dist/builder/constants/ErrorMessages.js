dbfcdc0ad1a928643fdb520f104e0a611bd0aa99
Veljko Blagojevic <veljko.blagojevic@transperfect.com>
2024-12-18 20:29:35 +0100

    final! test

--- dist/builder/constants/ErrorMessages.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorMessages = void 0;
-class ErrorMessages {
-    // Login Page
-    static INVALID_CREDENTIALS = 'Epic sadface: Username and password do not match any user in this service';
-    static LOCKED_OUT_USER = 'Epic sadface: Sorry, this user has been locked out.';
-}
-exports.ErrorMessages = ErrorMessages;
